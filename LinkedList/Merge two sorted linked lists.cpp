14210342430f0434c52197716b40d8e2b3730837
Amitkumar345 <71551492+Amitkumar345@users.noreply.github.com>
2021-05-11 00:50:59 +0530

    Create Merge two sorted linked lists.cpp

---
+++ LinkedList/Merge two sorted linked lists.cpp
@@ -0,0 +1,39 @@
+// LINK: https://practice.geeksforgeeks.org/problems/merge-two-sorted-linked-lists/1#
+////EXAMPLE
+// Input:
+// N = 4, M = 3 
+// valueN[] = {5,10,15,40}
+// valueM[] = {2,3,20}
+// Output: 2 3 5 10 15 20 40
+
+struct Node* SortedMerge(struct Node* a, struct Node* b)
+{
+    /*
+    // recursion
+    if(a==NULL)return b;
+    if(b==NULL)return a;
+    if(a->data < b->data){
+        a->next = SortedMerge(a->next,b);
+        return a;
+    }else{
+        b->next = SortedMerge(a,b->next);
+        return b;
+    }
+    */
+    
+    // second method using POINTER ON POINTER CONCEPT
+    struct Node* head = NULL;
+    struct Node** ref=&head;
+    while(a && b){
+        if(a->data < b->data){
+            *ref = a;
+            a=a->next;
+        }else{
+            *ref =b;
+            b= b->next;
+        }
+        ref=&((*ref)->next);
+    }
+    *ref=(a)?a:b;
+    return head;
+}
